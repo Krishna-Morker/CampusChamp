be07e3cbf32b77d6d2eb3b4bc27255c2f48b7ae1
Krishna-Morker <morkerkrishna83@gmail.com>
2024-11-01 15:23:27 +0530

    Completed all my and create course

--- app/api/course/route.js
+++ app/api/course/route.js
@@ -1,9 +1,17 @@
 import { message } from "antd";
 import { NextResponse } from "next/server";
 import { connect } from "@/lib/mongodb/mongoose";
-import {Addcourse, Getcourse} from "@/lib/actions/course"
+import {Addcourse, Getcourse, Mycourse} from "@/lib/actions/course"
 
-
+export async function GET(request) {
+  try {
+    console.log("svsrvrr")
+    const body =  request.query;
+    console.log(body,"get");
+    } catch (error) {
+      console.log('Error deleting user:', error);
+    }
+}
 
 export async function POST(request) {
     try {
@@ -17,6 +25,9 @@ export async function POST(request) {
         }else if(ge==="get"){
         //  console.log(body,"get");
           const res=await Getcourse(body.id);
+          return NextResponse.json(res);
+        }else if(ge==="mycou"){
+          const res=await Mycourse(body.id);
           return NextResponse.json(res);
         }
       } catch (error) {
