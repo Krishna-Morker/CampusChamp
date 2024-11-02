29c0850e250f7e2d881a6b91489df1423808e936
Krishna-Morker <morkerkrishna83@gmail.com>
2024-11-02 17:13:15 +0530

    Working on assignment upload by student and prof

--- app/api/assignment/route.js
+++ app/api/assignment/route.js
@@ -1,21 +1,28 @@
 import { NextResponse } from "next/server";
-import {Addassignment,getAssigment} from "@/lib/actions/assignment"
+import {Addassignment,getAssigment,Addstudent,delStudent} from "@/lib/actions/assignment"
 
 export async function POST(request) {
     try {
         const body = await request.json();
-
         const {ge}=body;
        // console.log(body,"fcwefewF")
-        if(ge=="add"){
-            const res=getAssigment(body)
-            console.log(res);
+        if(ge==="add"){
+            const res=await getAssigment(body);
             return NextResponse.json(res);
         }else if(ge==="addass"){
-        const res=Addassignment(body)
+        const res=await Addassignment(body)
         return NextResponse.json(res);
+        }else if(ge==="addstu"){
+          const {newFile}=body;
+          const res=await Addstudent(newFile)
+          return NextResponse.json(res);
+        }else if(ge==="del"){
+          const res=await Addstudent(body)
+          return NextResponse.json(res);
+        }else if(ge==="removestu"){
+          const res=await delStudent(body)
+          return NextResponse.json(res);
         }
-         
       } catch (error) {
         console.log('Error in course api', error);
       }
