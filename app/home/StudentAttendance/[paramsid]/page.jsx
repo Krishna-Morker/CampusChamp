c3c434e86163068ef7ad51851eba7c1805fea932
siddhbhalodia <bhalodiasid991010@gmail.com>
2024-11-05 15:00:09 +0530

    created UI for calendar view of attendance for student

--- app/home/StudentAttendance/[paramsid]/page.jsx
+++ app/home/StudentAttendance/[paramsid]/page.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 import { useUser } from '@clerk/nextjs';
 import { useRouter } from 'next/navigation';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
 export default function ViewAttendancePage({params}) {
     const router = useRouter();
     const { paramsid } = use(params);
@@ -12,7 +15,9 @@ export default function ViewAttendancePage({params}) {
     const [userId,setuserId]=useState(null);
     const [courseId,setCourseId] = useState(null)
     const [StudentAttendance,setStudentAttendance] = useState([])
-
+    const [presentDates,setPresentDates] = useState([])
+    const [absentDates,setAbsentDates] = useState([])
+    const [events,setEvents] = useState([])
     
     // Fetch Attendance from the backend
     useEffect(() => {
@@ -32,7 +37,51 @@ export default function ViewAttendancePage({params}) {
                 const attendance = await axios.post(`/api/attendance`,{courseId,userId: fg,ge});
 
                 if(attendance) setStudentAttendance(attendance.data)    
-                console.log(attendance.data)
+
+                const temp=attendance.data;
+                // console.log(temp)
+                
+                const pdates = temp.filter(record => record.status === "Present").map(record => record.date);
+                const pformattedDates = pdates.map(dateStr => {
+                    const date = new Date(dateStr);
+                    const year = date.getFullYear();
+                    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+                    const day = String(date.getDate()).padStart(2, '0');
+                    
+                    return `${year}-${month}-${day}`;
+                });
+                // console.log(pformattedDates)
+                if(pformattedDates) setPresentDates(pformattedDates);
+
+                const adates = temp.filter(record => record.status === "Absent").map(record => record.date);
+                const aformattedDates = adates.map(dateStr => {
+                    const date = new Date(dateStr);
+                    const year = date.getFullYear();
+                    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+                    const day = String(date.getDate()).padStart(2, '0');
+                    
+                    return `${year}-${month}-${day}`;
+                });
+                // console.log(aformattedDates)
+                if(aformattedDates) setAbsentDates(aformattedDates);
+
+                const allevents = [
+                    ...pformattedDates.map(date => ({
+                    title: "Present",
+                    start: (date),
+                    color: "green" // Color for present dates
+                    })),
+                    ...aformattedDates.map(date => ({
+                    title: "Absent",
+                    start: (date),
+                    color: "red" // Color for absent dates
+                    }))
+                ];
+
+                if(allevents) setEvents(allevents);
+
+                console.log(allevents)
+
             } catch (error) {
                 console.error('Error fetching attendance:', error);
             }
@@ -46,7 +95,17 @@ export default function ViewAttendancePage({params}) {
     
     return (
         <>
-            <div>hello</div>
+            <h1 className="text-center text-2xl font-semibold text-red-800 mb-6">Course Attendance</h1>
+            <FullCalendar
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                events={events}
+                headerToolbar={{
+                left: 'prev,next today',
+                center: 'title',
+                right: 'dayGridMonth,dayGridWeek,dayGridDay'
+                }}
+            />
         </>
     )
 }
